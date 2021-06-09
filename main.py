3d03f777b733af080c43742bcdcc5da340deead8
Danilo Lemes <daniloleemes@hotmail.com>
2021-06-09 22:47:24 +0100

    add test log

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import praw, discord, os
 from datetime import datetime
 
+print(os.environ)
 client_id = os.environ.get("client_id")
 client_secret = os.environ.get("client_secret")
 client_key = os.environ.get("client_key")
